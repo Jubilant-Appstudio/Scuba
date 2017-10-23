5d320b65527c9ba7a8555a9f8e0a621b6c25a760
“Mahipal” <jubilantappstudio@gmail.com>
2017-10-23 22:37:16 +0530

    swiftlint

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+
+target 'ScubaCalendar' do
+  use_frameworks!
+
+	pod 'SwiftLint'
+end
---
+++ ScubaCalendar/Controller/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ScubaCalendar
+//
+//  Created by Mahipalsinh on 10/23/17.
+//  Copyright © 2017 CodzGarage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
